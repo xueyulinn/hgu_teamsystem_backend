6dd4b837cc9cf4a202cfe9ee65e881f6991c5cf9
xueyulin <xueyulin2001@163.com>
2023-11-17 11:00:55 +0800

    Initial commit 项目初始化

---
+++ src/main/java/com/hgu/usercenter/contant/UserConstant.java
@@ -0,0 +1,29 @@
+package com.hgu.usercenter.contant;
+
+/**
+ * 用户常量
+ *
+ * @author <a href="https://github.com/xueyulinn">薛钰麟</a>
+ */
+public interface UserConstant {
+
+    /**
+     * 用户登录态键
+     */
+    String USER_LOGIN_STATE = "userLoginState";
+
+    //  ------- 权限 --------
+
+    /**
+     * 默认权限
+     */
+    int DEFAULT_ROLE = 0;
+
+    // https://space.bilibili.com/12890453/
+
+    /**
+     * 管理员权限
+     */
+    int ADMIN_ROLE = 1;
+
+}
---
+++ src/main/java/com/hgu/usercenter/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.hgu.usercenter.exception;
+
+import com.hgu.usercenter.common.BaseResponse;
+import com.hgu.usercenter.common.ErrorCode;
+import com.hgu.usercenter.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author <a href="https://github.com/xueyulinn">薛钰麟</a>
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtils.error(e.getCode(), e.getMessage(), e.getDescription());
+    }
+
+    //https://github.com/liyupi
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), "");
+    }
+}
